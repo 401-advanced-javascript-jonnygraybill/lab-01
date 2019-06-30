7f897a5579b2c232296458978718c6c3fdcea689
Jonny Graybill <jon.a.graybill@gmail.com>
2019-06-29 19:52:38 -0700

    refactored

--- __tests__/arithmetic.test.js
+++ __tests__/arithmetic.test.js
@@ -2,3 +2,30 @@
 
 const arithmetic = require('../lib/arithmetic.js');
 
+// let methods = ['add', 'subtract', 'multiply', 'divide'];
+
+describe('Add', () => {
+
+  it('adds positive numbers', () => {
+    let result = arithmetic.add(1, 10, 100, 1000);
+    expect(result).toBe(1111);
+  });
+
+  it('adds negative numbers', () => {
+    let result = arithmetic.add(-1, -10, -100, -1000);
+    expect(result).toBe(-1111);
+  });
+
+  it('adds fractions', () => {
+    let result = arithmetic.add(0.1, 0.2);
+    expect(result).toBe(0.3);
+  });
+
+});
+
+
+
+
+
+
+

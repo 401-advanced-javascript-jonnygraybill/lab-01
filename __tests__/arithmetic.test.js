52f1cbb12552b9419b610a3eb670a1fd9227ed8f
Jonny Graybill <jon.a.graybill@gmail.com>
2019-06-29 21:59:55 -0700

    finished

--- __tests__/arithmetic.test.js
+++ __tests__/arithmetic.test.js
@@ -1,31 +1,46 @@
 'use strict';
 
 const arithmetic = require('../lib/arithmetic.js');
-
-// let methods = ['add', 'subtract', 'multiply', 'divide'];
+let faker = require('faker');
 
 describe('Add', () => {
-
   it('adds positive numbers', () => {
-    let result = arithmetic.add(1, 10, 100, 1000);
-    expect(result).toBe(1111);
-  });
-
-  it('adds negative numbers', () => {
-    let result = arithmetic.add(-1, -10, -100, -1000);
-    expect(result).toBe(-1111);
-  });
-
-  it('adds fractions', () => {
-    let result = arithmetic.add(0.1, 0.2);
-    expect(result).toBe(0.3);
-  });
-
+    let result1 = (faker.random.number()); 
+    let result2 = (faker.random.number());
+    expect(arithmetic.add(result1,result2)).toBe(result1+result2);
+  });  
 });
 
+describe('Subtract', () => {
+  it('subtracts numbers', () => {
+    let result1 = (faker.random.number());
+    let result2 = (faker.random.number());
+    expect(arithmetic.subtract(result1,result2)).toBe(result1-result2);
+  });
+});
 
+describe('Multiply', () => {
+  it('multiplies numbers', () => {
+    let result1 = (faker.random.number());
+    let result2 = (faker.random.number());
+    expect(arithmetic.multiply(result1,result2)).toBe(result1*result2);
+  });
 
+  it('Returns 0 when either input === 0', () => {
+    let result1 = (faker.random.number());
+    expect(arithmetic.multiply(result1,0)).toBe(0);
+  });
+});
 
+describe('Divide', () => {
+  it('divide numbers', () => {
+    let result1 = (faker.random.number());
+    let result2 = (faker.random.number());
+    expect(arithmetic.divide(result1,result2)).toBe(result1/result2);
+  });
 
-
-
+  it('Returns 0 when the second input === 0', () => {
+    let result2 = (faker.random.number());
+    expect(arithmetic.divide(0,result2)).toBe(0);
+  });
+});
